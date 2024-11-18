7a7a067667587b3856b86ae33906adb51e536a08
jvras <jonathas.vinicius@ufpe.br>
2024-11-18 13:56:34 -0300

    layout google datastudio

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,23 +1,49 @@
 "use client";
 import React from "react";
-import ChartThree from "../Charts/ChartThree";
 import ChartTwo from "../Charts/ChartTwo";
-import TableOne from "../Tables/TableOne";
 import DataStatsOne from "@/components/DataStats/DataStatsOne";
 import ChartOne from "@/components/Charts/ChartOne";
+import TableOne from "../Tables/TableOne";
+import MapOne from "../Maps/MapOne";
 
 const Dashboard: React.FC = () => {
   return (
     <>
+      <div className="flex justify-center items-center mb-6">
+        <h2 className="text-heading-2 dark:text-white">Investimento</h2>
+      </div>
+      <DataStatsOne />
+
+      <div className="flex justify-center items-center mb-6">
+        <h2 className="text-heading-2 dark:text-white">GGR</h2>
+      </div>
+      <DataStatsOne />
+
+      <div className="flex justify-center items-center mb-6">
+        <h2 className="text-heading-2 dark:text-white">Cadastro</h2>
+      </div>
       <DataStatsOne />
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-9 2xl:gap-7.5">
         <ChartOne />
         <ChartTwo />
-        <ChartThree />
-        <div className="col-span-12 xl:col-span-8">
-          <TableOne />
+        <div className="col-span-12 flex justify-center items-center min-h-[400px]">
+          <MapOne />
         </div>
+      </div>
+        
+      <div className="flex justify-center items-center mb-6">
+        <h2 className="text-heading-2 dark:text-white">Depósito</h2>
+      </div>
+      <div id="1" className="mb-6">
+        <DataStatsOne />
+      </div>
+      <div id="2" className="mb-12 text-heading-2">
+        <DataStatsOne />
+      </div>
+
+      <div className="col-span-12 xl:col-span-8">
+        <TableOne />
       </div>
     </>
   );
