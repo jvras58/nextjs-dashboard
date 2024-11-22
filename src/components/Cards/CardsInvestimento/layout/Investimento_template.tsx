79f495115284fa5e406e3c50bc2435b86abcec27
jvras <jonathas.vinicius@ufpe.br>
2024-11-22 12:45:04 -0300

    feat: atualiza layout dos componentes de cartões de Cadastro e Investimento no Dashboard

--- src/components/Cards/CardsInvestimento/layout/Investimento_template.tsx
+++ src/components/Cards/CardsInvestimento/layout/Investimento_template.tsx
@@ -3,13 +3,14 @@ import CardTotalInvestido from "../CardTotalInvestimento";
 import CardMedia_dia_investimento from "../CardMediaInvestimento";
 import CardRoi from "../CardRoi";
 
-// TODO: colocar mais no meio da tela como são 3 cards
 const InvestimentoCards: React.FC = () => {
   return (
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
-      <CardTotalInvestido />
-      <CardMedia_dia_investimento />
-      <CardRoi />
+    <div className="flex justify-center">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
+        <CardTotalInvestido />
+        <CardMedia_dia_investimento />
+        <CardRoi />
+      </div>
     </div>
   );
 };
