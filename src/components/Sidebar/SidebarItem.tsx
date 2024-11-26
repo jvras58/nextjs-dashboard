5bb0fb264de6119f6e2e31b5ed9618f19e56aa55
jvras <jvras@cin.ufpe.br>
2024-11-26 16:02:38 -0300

    feat: atualiza SidebarItem para usar usePathname e remove lógica de clique

--- src/components/Sidebar/SidebarItem.tsx
+++ src/components/Sidebar/SidebarItem.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const SidebarItem = ({ item, pageName, setPageName }: any) => {
-  const handleClick = () => {
-    setPageName(item.label.toLowerCase());
-  };
+const SidebarItem = ({ item }: any) => {
+  const pathname = usePathname();
 
   return (
     <li>
       <Link
         href={item.route}
-        onClick={handleClick}
         className={`${
-          pageName === item.label.toLowerCase()
+          pathname === item.route
             ? "bg-primary/[.07] text-primary dark:bg-white/10 dark:text-white"
             : "text-dark-4 hover:bg-gray-2 hover:text-dark dark:text-gray-5 dark:hover:bg-white/10 dark:hover:text-white"
         } group relative flex items-center gap-3 rounded-[7px] px-3.5 py-3 font-medium duration-300 ease-in-out`}
