b1b3507996d1083955d2b7e6d3d2db94a948238e
jvras <jvras@cin.ufpe.br>
2024-11-27 10:53:21 -0300

    feat: adiciona loader ao componente AfiliadosList durante o carregamento dos dados

--- src/components/Cards/Afiliado/Afiliados-List.tsx
+++ src/components/Cards/Afiliado/Afiliados-List.tsx
@@ -2,9 +2,14 @@
 
 import useFiliadoList from "@/hooks/useFiliadoList";
 import AfiliadoCard from "@/components/Cards/Afiliado/afiliado";
+import Loader from "@/components/common/Loader";
 
 const AfiliadosList = () => {
-const { data } = useFiliadoList("cadastro");
+const { data, loading } = useFiliadoList("cadastro");
+
+if (loading) {
+return <Loader />;
+}
 
 return (
 <div className="grid gap-4 grid-cols-1 md:grid-cols-3 p-6">
