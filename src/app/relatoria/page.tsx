a76d0b99bdc8097529c6362d230ede91ad066583
phvm <pedrohvdm@gmail.com>
2024-11-28 15:31:51 -0300

    feat: relatoria page conditional render

--- src/app/relatoria/page.tsx
+++ src/app/relatoria/page.tsx
@@ -1,54 +1,13 @@
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import ArquivoUpload from "@/components/upload/upload";
+import Relatoria from "@/components/Relatoria";
 import { Metadata } from "next";
-import { Button } from "@/components/ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog";
 
 export const metadata: Metadata = {
   title: "Relatório - Betinha",
   description: "Betinha Web",
 };
 
-const Relatoria = () => {
-  return (
-    <DefaultLayout HeaderTitle="Relatório">
-      <h1 className="text-center text-5xl font-semibold">Relatório</h1>
-      <p className="mb-20 text-center text-muted-foreground md:mb-40">
-        Upload de arquivos para relatórios
-      </p>
-      <ArquivoUpload />
-
-      {/*<div className="flex gap-4">*/}
-      {/*    <Dialog>*/}
-      {/*    <DialogTrigger asChild>*/}
-      {/*        <Button className="rounded-full shadow" variant="outline">*/}
-      {/*        Upload de Arquivos*/}
-      {/*        </Button>*/}
-      {/*    </DialogTrigger>*/}
-      {/*    <DialogContent className="sm:max-w-[425px]">*/}
-      {/*        <DialogHeader>*/}
-      {/*        <DialogTitle className="text-center">*/}
-      {/*            Envie seus arquivos*/}
-      {/*        </DialogTitle>*/}
-      {/*        <DialogDescription className="text-center">*/}
-      {/*            Envie Arquivos CSV*/}
-      {/*        </DialogDescription>*/}
-      {/*        </DialogHeader>*/}
-      {/*        <div className="grid gap-4 py-4">*/}
-      {/*        <ArquivoUpload />*/}
-      {/*        </div>*/}
-      {/*    </DialogContent>*/}
-      {/*    </Dialog>*/}
-      {/*</div>*/}
-    </DefaultLayout>
-  );
+const Page = () => {
+  return <Relatoria />;
 };
 
-export default Relatoria;
+export default Page;
---
+++ src/components/Relatoria/index.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { useState } from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import ArquivoUpload from "@/components/upload/upload";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+
+export default function Relatoria() {
+  const [hasFile, setHasFile] = useState<boolean>(false);
+
+  console.log(hasFile);
+
+  if (!hasFile) {
+    return (
+      <DefaultLayout HeaderTitle="Relatório">
+        <h1 className="text-center text-5xl font-semibold">Relatório</h1>
+        <p className="mb-20 text-center text-muted-foreground md:mb-40">
+          Upload de arquivos para relatórios
+        </p>
+        <ArquivoUpload />
+      </DefaultLayout>
+    );
+  }
+  return (
+    <DefaultLayout HeaderTitle="Relatório">
+      <div className="flex gap-4">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button className="rounded-full shadow" variant="outline">
+              Upload de Arquivos
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle className="text-center">
+                Envie seus arquivos
+              </DialogTitle>
+              <DialogDescription className="text-center">
+                Envie Arquivos CSV
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <ArquivoUpload />
+            </div>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </DefaultLayout>
+  );
+}
