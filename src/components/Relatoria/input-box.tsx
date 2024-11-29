375352d617a5f62ef31d5a478055b057d19c6435
jvras <jvras@cin.ufpe.br>
2024-11-29 19:20:07 -0300

    feat: adiciona componente CSVTable e integra na InputBox para exibir dados da planilha

--- src/components/Relatoria/input-box.tsx
+++ src/components/Relatoria/input-box.tsx
@@ -2,12 +2,11 @@
 
 import getDocs from "@/actions/spreadsheets-actions/getDocs";
 import { useState } from "react";
+import CSVTable from "../Tables/Csv-table";
 
 const InputBox: React.FC = () => {
-const [id, setId] = useState<string>("");
-const [result, setResult] = useState<{ title: string; csv: string } | null>(
-null
-);
+const [id, setId] = useState<string>("1NMZv6FsBUdzJLoEozSyOtscnGVnkGkBPng9xrtsRwLc");
+const [result, setResult] = useState<{ title: string; csv: string } | null>(null);
 
 const handleSubmit = async (event: React.FormEvent) => {
 event.preventDefault();
@@ -21,15 +20,13 @@ try {
 
 return (
 <div className="w-full max-w-2xl mx-auto p-6 bg-white dark:bg-gray-800 shadow-md rounded-lg">
-    <form
-    onSubmit={handleSubmit}
-    className="flex flex-col space-y-4 items-center"
-    >
+    <form onSubmit={handleSubmit} className="flex flex-col space-y-4 items-center">
     <input
         type="text"
         value={id}
         onChange={(e) => setId(e.target.value)}
         placeholder="Digite o ID da Planilha"
+        required
         className="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-gray-700 dark:text-gray-200"
     />
     <button
@@ -39,18 +36,7 @@ return (
         Enviar
     </button>
     </form>
-    {result && (
-    <div className="mt-6">
-        <h2 className="text-lg font-bold text-gray-800 dark:text-gray-100">
-        {result.title}
-        </h2>
-        <div className="mt-4 p-4 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-lg overflow-x-auto">
-        <pre className="whitespace-pre-wrap text-gray-800 dark:text-gray-100">
-            {result.csv}
-        </pre>
-        </div>
-    </div>
-    )}
+    {result && <CSVTable csvData={result.csv} title={result.title} />}
 </div>
 );
 };
