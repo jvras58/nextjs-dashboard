427a00907203fe9cfa9c636c785d4c1d915dddf2
jvras <jvras@cin.ufpe.br>
2024-12-21 14:06:16 -0300

    adiciona página de cadastro com verificação de sessão para administradores e refatora layout

--- src/hooks/useRole.ts
+++ src/hooks/useRole.ts
@@ -2,9 +2,6 @@ import { useSession } from "next-auth/react";
 
 export function useRole() {
   const { data: session } = useSession();
-
-  console.log("Session Data:", session);
-
   const isAdmin = () => session?.user?.role === 'admin';
   
   const hasRole = (role: string) => session?.user?.role === role;
